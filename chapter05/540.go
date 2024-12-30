43878fe7b11cc01e798f5d6ac19e69095555ba01
Kyohei Saito <k01207s@gmail.com>
2024-12-30 14:33:03 +0900

    5.4 defer

---
+++ chapter05/540.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		log.Fatal("File is not specified.")
+	}
+	f, err := os.Open(os.Args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	data := make([]byte, 2048)
+	for {
+		count, err := f.Read(data)
+		os.Stdout.Write(data[:count])
+		if err != nil {
+			if err != io.EOF {
+				log.Fatal(err)
+			} else {
+				fmt.Println("End of File")
+			}
+			break
+		}
+	}
+}
