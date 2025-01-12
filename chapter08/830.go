eed31715614ea29fbc3312ce8ae7968ede61a435
Kyohei Saito <k01207s@gmail.com>
2025-01-12 16:47:01 +0900

    8.3 Sentinel error

---
+++ chapter08/830.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"archive/zip"
+	"bytes"
+	"fmt"
+)
+
+func main() {
+	data := []byte("This is not a zip file")
+	nonZipFile := bytes.NewReader(data)
+	_, err := zip.NewReader(nonZipFile, int64(len(data)))
+
+	if err == zip.ErrFormat {
+		fmt.Println("This is not a zip format")
+	}
+}
