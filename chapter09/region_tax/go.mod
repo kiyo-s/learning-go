db4f15fc8158d24c65a7f52a572059364f82f8d9
Kyohei Saito <k01207s@gmail.com>
2025-02-24 18:47:54 +0900

    9.4.5 upgrade to not compatible package

--- chapter09/region_tax/go.mod
+++ chapter09/region_tax/go.mod
@@ -3,6 +3,6 @@ module example.co.jp/region_tax
 go 1.23.6
 
 require (
-	github.com/learning-go-book/simpletax v1.0.0
+	github.com/learning-go-book/simpletax/v2 v2.0.0
 	github.com/shopspring/decimal v1.4.0
 )
--- chapter09/region_tax/main.go
+++ chapter09/region_tax/main.go
@@ -2,20 +2,24 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 
-	"github.com/learning-go-book/simpletax" //liststart1
-	"github.com/shopspring/decimal"         //listend1
+	"github.com/learning-go-book/simpletax/v2"
+	"github.com/shopspring/decimal"
 )
 
-func main() {
-	amount, _ := decimal.NewFromString(os.Args[1])
-	zip := os.Args[2]
-	percent, err := simpletax.TaxForZip(zip)
+func main() { //liststart1
+	amount, err := decimal.NewFromString(os.Args[1])
 	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+		log.Fatal(err)
+	}
+	zip := os.Args[2]
+	country := os.Args[3]
+	percent, err := simpletax.ForCountryPostalCode(country, zip)
+	if err != nil {
+		log.Fatal(err)
 	}
 	total := amount.Add(amount.Mul(percent)).Round(2)
 	fmt.Println(total)
-}
+} //listend1
