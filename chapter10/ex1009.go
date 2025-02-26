004aa319f7169e496f86c4c89679b84167497cfc
Kyohei Saito <k01207s@gmail.com>
2025-02-26 21:33:41 +0900

    10.5.6 channel with buffer

---
+++ chapter10/ex1009.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func processChannel(ch chan int) []int {
+	const maxConc = 10
+	results := make(chan int, maxConc)
+
+	for i := 0; i < maxConc; i++ {
+		go func() {
+			v := <-ch
+			results <- process(v)
+		}()
+	}
+	fmt.Println("Complete run gorutine")
+
+	var out []int
+	for i := 0; i < maxConc; i++ {
+		out = append(out, <-results)
+	}
+	return out
+}
+
+func process(v int) int {
+	returnVal := v * v
+	rand.Seed(time.Now().UnixNano())
+	sleepSec := rand.Intn(3)
+
+	fmt.Println("process: ", v, returnVal, sleepSec)
+	time.Sleep(time.Duration(sleepSec) * time.Second)
+	return returnVal
+}
+
+func main() {
+	ch := make(chan int)
+
+	var result []int
+
+	go func() {
+		for i := 0; i < 100; i++ {
+			ch <- i
+		}
+	}()
+
+	fmt.Println(ch)
+
+	result = processChannel(ch)
+
+	fmt.Printf("result: %d\n", result)
+}
