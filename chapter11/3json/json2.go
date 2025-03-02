3aace41280f465d986debbd3a9d635ffe02bb2ee
Kyohei Saito <k01207s@gmail.com>
2025-03-02 15:30:43 +0900

    11.3.1 annotate metadata by structured tag

---
+++ chapter11/3json/json2.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+	"time"
+)
+
+type Order struct {
+	ID          string    `json:"id"`
+	DateOrdered time.Time `json:"date_ordered"`
+	CustomerID  string    `json:"customer_id"`
+	Items       []Item    `json:"items"`
+}
+
+type Item struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
+}
+
+func main() {
+	data := readData()
+	var o Order
+	err := json.Unmarshal(data, &o)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("%+v\n", o)
+}
+
+func readData() []byte {
+	b, err := os.ReadFile("testdata/data.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+	return b
+}
