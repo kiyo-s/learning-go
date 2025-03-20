326686fa8343f9db15310bcd5e2aa26b69c256c8
Kyohei Saito <k01207s@gmail.com>
2025-03-20 17:16:06 +0900

    13.1.1 reporting about test fail

--- chapter13/adder/adder_test.go
+++ chapter13/adder/adder_test.go
@@ -5,6 +5,6 @@ import "testing"
 func Test_addNumbers(t *testing.T) {
 	result := addNumbers(2, 3)
 	if result != 5 {
-		t.Error("Invalid result: expected 5, got", result)
+		t.Errorf("Invalid result: expected %d, got %d", 5, result)
 	}
 }
