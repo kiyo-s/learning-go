c2385c1dc58aef32a4883610bc18886d3c2a283b
Kyohei Saito <k01207s@gmail.com>
2025-03-20 17:25:54 +0900

    13.1.2 prepare and clean up

--- chapter13/adder/adder_test.go
+++ chapter13/adder/adder_test.go
@@ -1,6 +1,29 @@
 package adder
 
-import "testing"
+import (
+	"fmt"
+	"os"
+	"testing"
+	"time"
+)
+
+var testTime time.Time
+
+func TestMain(m *testing.M) {
+	fmt.Println("Preparing for tests")
+	testTime = time.Now()
+	exitVal := m.Run()
+	fmt.Println("Cleaning up after tests")
+	os.Exit(exitVal)
+}
+
+func TestFirst(t *testing.T) {
+	fmt.Println("TestFirst uses what was set in TestMain:", testTime)
+}
+
+func TestSecond(t *testing.T) {
+	fmt.Println("TestSecond also uses what was set in TestMain:", testTime)
+}
 
 func Test_addNumbers(t *testing.T) {
 	result := addNumbers(2, 3)
